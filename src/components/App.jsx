67ded0835bd1d8a3db787fcbd954c63c417e5740
Nadiia Tsytsylina <miniova95@gmail.com>
2024-02-25 19:10:55 +0200

    updated App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,43 @@
-import logo from 'logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+
+import SendReport from 'pages/SendReport';
+import AllReports from 'pages/AllReports';
+import Layout from './Layout/Layout';
+
+import { selectToken } from '../redux/selectors';
+import { setToken } from '../redux/tokenSlice';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
+  const dispatch = useDispatch();
+  const clientToken = useSelector(selectToken);
+
+  const generateToken = () => {
+    const token =
+      Math.random().toString(36).substr(2) +
+      Math.random().toString(36).substr(2);
+    return token;
+  };
+
+  useEffect(() => {
+    if (!clientToken) {
+      dispatch(setToken(generateToken()));
+    }
+  }, [clientToken, dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<SendReport />} />
+        <Route path="reports" element={<AllReports />} />
+      </Route>
+    </Routes>
   );
 }
 
