6e6cffbd5ec7e62f275b4d7e2e871f782b1edd5a
Nadiia Tsytsylina <miniova95@gmail.com>
2024-02-26 14:27:22 +0200

    Update SendReport.jsx

--- src/pages/SendReport.jsx
+++ src/pages/SendReport.jsx
@@ -1,10 +1,10 @@
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import ReportForm from 'components/ReportForm/ReportForm';
-import { REACT_APP_SECRET_KEY } from 'constants/constants';
+import { REACT_APP_SITE_KEY } from 'constants/constants';
 
 export default function SendReport() {
   return (
-    <GoogleReCaptchaProvider reCaptchaKey={REACT_APP_SECRET_KEY}>
+    <GoogleReCaptchaProvider reCaptchaKey={REACT_APP_SITE_KEY}>
       <ReportForm />;
     </GoogleReCaptchaProvider>
   );
